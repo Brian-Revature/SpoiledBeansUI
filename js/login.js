36f29ddbadbb7c14bfbaa08269b73e05d0f168b7
Gerts19 <gregory.gertsen@revature.net>
2021-03-17 20:49:28 -0700

    Home page and Movie page

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     document.getElementById("login-button").addEventListener('click', grabUsername);
+
 }
 
 import {state} from './state.js';
@@ -26,24 +27,68 @@ function grabUsername() {
     })
         .then(response => {
             state.token = response.headers.get("spoiledBeans-token");
+            window.localStorage.setItem('token', response.headers.get("spoiledBeans-token"));
+
     })
         .then(() => redirect());
 }
 
 function redirect(){
-    let url = 'http://Spoiledbeansapi-env.eba-mnv79iji.us-east-2.elasticbeanstalk.com/users';
-    let headers = new Headers();
 
-    headers.append('Content-Type', 'application/json');
-    headers.append('Accept', 'application/json');
-    headers.append('spoiledBeans-token', state.token);
 
-    fetch(url, {
-        method: 'GET',
-        headers: headers
-    })
-        .then(response => response.json())
-        .then(result => {
-            console.log('Success:', result);
-    });
+        let url = 'http://Spoiledbeansapi-env.eba-mnv79iji.us-east-2.elasticbeanstalk.com/users';
+        let url2 = 'http://Spoiledbeansapi-env.eba-mnv79iji.us-east-2.elasticbeanstalk.com/users/myfavorites';
+        let url3 = 'http://Spoiledbeansapi-env.eba-mnv79iji.us-east-2.elasticbeanstalk.com/movies';
+        let url4 = 'http://Spoiledbeansapi-env.eba-mnv79iji.us-east-2.elasticbeanstalk.com/reviews/myreviews';
+
+        let headers = new Headers();
+
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+        headers.append('spoiledBeans-token', state.token);
+
+
+        fetch(url, {
+            method: 'GET',
+            headers: headers
+        })
+            .then(response => response.json())
+            .then(result => {
+                console.log('Success:', result);
+        });
+
+//        fetch(url2, {
+//                    method: 'GET',
+//                    headers: headers
+//                })
+//                    .then(response => response.json())
+//                    .then(result => {
+//                        console.log('Success:', result);
+//                });
+//
+//
+//
+//         fetch(url3, {
+//                           method: 'GET',
+//                           headers: headers
+//                       })
+//                           .then(response => response.json())
+//                           .then(result => {
+//                               console.log('Success:', result);
+//                       });
+//
+//        fetch(url4, {
+//                            method: 'GET',
+//                            headers: headers
+//                        })
+//                            .then(response => response.json())
+//                            .then(result => {
+//                                console.log('Success:', result);
+//                        });
+
+
+        //document.getElementById("tH").click();
+        //window.location.href-"..html/home.html";
+        document.location.href="../html/home.html";
+
 }
